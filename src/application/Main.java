595f24ab7ff78fb4261679e50666f682a1c83a28
Marina <marinaalcaidec99@gmail.com>
2021-12-11 23:13:55 +0100

    primera versión interfaz pagina login

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,33 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/login.fxml"));
+			controller_login control = new controller_login();
+			loader.setController(control);
+			Parent root = loader.load();
+			//BorderPane root = new BorderPane();
+			//Scene scene = new Scene(root, 400, 400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//primaryStage.setScene(scene);
+			primaryStage.setScene(new Scene(root));
+			//primaryStage.setResizable(false);
+			primaryStage.sizeToScene();
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/application/controller_login.java
@@ -0,0 +1,44 @@
+
+package application;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class controller_login {
+
+    @FXML
+    private AnchorPane loginPage;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private JFXButton forgetButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField userField;
+    
+    @FXML
+    void deleteText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void forgetFunction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginFunction(ActionEvent event) {
+
+    }
+
+}
