70dac136c708c4e8ba11c353fae8be4873ec5db1
Marina <marinaalcaidec99@gmail.com>
2021-12-12 02:25:11 +0100

    feat: improve the login page

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,12 +15,7 @@ public class Main extends Application {
 			controller_login control = new controller_login();
 			loader.setController(control);
 			Parent root = loader.load();
-			//BorderPane root = new BorderPane();
-			//Scene scene = new Scene(root, 400, 400);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//primaryStage.setScene(scene);
 			primaryStage.setScene(new Scene(root));
-			//primaryStage.setResizable(false);
 			primaryStage.sizeToScene();
 			primaryStage.show();
 		} catch (Exception e) {
