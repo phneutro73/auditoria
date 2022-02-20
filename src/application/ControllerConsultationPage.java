03214205a907966056c4a8e7072faba501dae89b
Marina Alcaide <marina.alcaide@eb2.tech>
2022-02-20 18:59:29 +0100

    fix: hide admin page in consultation page

--- src/application/ControllerConsultationPage.java
+++ src/application/ControllerConsultationPage.java
@@ -124,6 +124,10 @@ public class ControllerConsultationPage {
 
 	@FXML
 	void initialize() {
+		
+		if (currentUser.getRoleId() != 2) {
+			btnAdministrator.setVisible(false);
+		}
 
 		ConsultationPageConnection consultDB = new ConsultationPageConnection();
 
